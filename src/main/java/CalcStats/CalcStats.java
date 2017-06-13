4cfeaea182e53cf46d8fe45b5670533927138785
Kumpanat Phewphong <kumpanat_phe@truecorp.co.th>
2017-06-13 11:02:38 +0700

    fix refactor code

--- src/main/java/CalcStats/CalcStats.java
+++ src/main/java/CalcStats/CalcStats.java
@@ -8,14 +8,14 @@ import java.util.Arrays;
  */
 public class CalcStats {
 
-    public String[] cals(String sequence){
+    public String[] cals(String sequence) {
         String[] result = new String[4];
-        String newNumbers = sequence.replace(("\n"),",");
+        String newNumbers = sequence.replace(("\n"), ",");
         String[] numbers = newNumbers.split(",");
         int y = numbers.length;
-        double sum=0;
+        double sum = 0;
         int cal[] = new int[y];
-        for(int i=0;i<y;i++){
+        for (int i = 0; i < y; i++) {
             cal[i] = convertStringNum(numbers[i]);
             sum += cal[i];
         }
@@ -23,21 +23,20 @@ public class CalcStats {
         DecimalFormat df = new DecimalFormat();
         df.applyPattern("0.00");
         result[0] = String.valueOf(cal[0]);
-        result[1] = String.valueOf(cal[y-1]);
+        result[1] = String.valueOf(cal[y - 1]);
         result[2] = String.valueOf(y);
-        result[3] = String.valueOf(df.format(sum/y));
-        for(String output:result){
+        result[3] = String.valueOf(df.format(sum / y));
+        for (String output : result) {
             System.out.println(output);
         }
         return result;
     }
 
-    public int convertStringNum(String stringNum){
+    public int convertStringNum(String stringNum) {
         int intNumber;
-        try{
+        try {
             intNumber = Integer.parseInt(stringNum);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             intNumber = 0;
         }
         return intNumber;
